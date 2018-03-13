8ec1f7f46c6543cce89554357eebbbf2b572e969
Melito <bmelito@mitre.org>
2018-03-13 13:58:07 -0400

    Tasks complete

---
+++ README.md
@@ -0,0 +1,3 @@
+Try to solve all of the challenges in javscriptExercises.js!
+
+Feel free to run your code by using `node javascriptExercises.js` from the command line, or alternatively you can copy-paste your code into repl.it
---
+++ javascriptExercises.js
@@ -0,0 +1,57 @@
+let firstObject = {
+	firstItem : "Yo"
+}
+
+let secondObject = {
+	firstItem : "Hello",
+	secondItem : 3,
+}
+
+// Task One
+// For this task, create a new key in secondObject that points to firstObject.
+// After you've done this, console.log secondObject. It should look like this:
+// {
+//		firstItem : "Hello",
+//		secondITem : 3,
+//		thirdItem : {
+//				firstItem : "Yo"
+//		}
+// }
+
+// As a challenge(!), try creating your object to add to secondObject. Give this new object the key
+// fourthItem. Once you've added this object, try giving it it's own key called firstItem that 
+// points to the string "Greetings". Now when you console.log secondObject, it should look like this:
+// {
+//		firstItem : "Hello",
+//		secondITem : 3,
+//		thirdItem : {
+//				firstItem : "Yo"
+//		},
+//		fourthItem : {
+//			firstItem : "Greetings"
+//		}
+// }
+
+let firstArray = [1, "two", 3]
+
+// Task Two
+// For this task, create your own array called secondArray. You can put anything you want in the first 2 items,
+// but for the third item, have it point to the third item from firstArray. Console.log your answer
+
+// Task Three
+// Create a function that takes two numbers as parameters. You can call them something like 
+// numberOne and numberTwo in the function definition. Inside the function, add the two numbers
+// together and console.log the result. Call the function, and pass in the numbers 1 and 2 as arguments
+// (recall the vocabulary we went over!)
+
+let numbersArray = [1,2,3,4,5]
+
+// Task Four
+// Write a for loop that adds starts at the second number in the array, and adds the current number it
+// is looking at to the number that came before it. Console.log this result. Your answer should look like this:
+// 3
+// 5
+// 7
+// 9
+
+
